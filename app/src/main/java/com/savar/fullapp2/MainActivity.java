f2ced2646ae70e81208ef24e15b8be7d8b8692c7
mehdi <mehdi.behjati048@gmail.com>
2019-11-13 16:58:56 +0330

    chenge2

--- app/src/main/java/com/savar/fullapp2/MainActivity.java
+++ app/src/main/java/com/savar/fullapp2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.savar.fullapp2;
 
+import android.os.Bundle;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
 }
