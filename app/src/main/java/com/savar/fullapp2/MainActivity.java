53f999080e5ae7b636494de2774cdd42bef88e63
mehdi <mehdi.behjati048@gmail.com>
2019-11-15 03:14:02 +0330

    Chenge_5

--- app/src/main/java/com/savar/fullapp2/MainActivity.java
+++ app/src/main/java/com/savar/fullapp2/MainActivity.java
@@ -1,8 +1,13 @@
 package com.savar.fullapp2;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -13,6 +18,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        ALERT __ DIALOG
+
+        Button btnAlert = findViewById(R.id.btnAlert);
+        btnAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+
+                        .setTitle("UPDATE")
+                        .setMessage("please your update...")
+                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "UPDATE...", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "NO UPDATE...!", Toast.LENGTH_SHORT).show();
+                            }
+                        }).create();
+                dialog.show();
+            }
+        });
 
 
     }
