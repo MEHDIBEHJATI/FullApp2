f73cbd19cf907bcd3905015a93214ce0689032ac
mehdi <mehdi.behjati048@gmail.com>
2019-11-14 03:33:35 +0330

    chenge4

---
+++ app/src/main/java/com/savar/fullapp2/ShareTxtActivity.java
@@ -0,0 +1,14 @@
+package com.savar.fullapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShareTxtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_txt);
+    }
+}
